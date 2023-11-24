30fd74c7436697f8c513e8f53c905e6958cfb25f
kukkadapudheeraj@gmail.com <kukkadapudheeraj>
2023-11-23 21:49:22 -0500

    classification completed

--- chatbot/services/classifier.py
+++ chatbot/services/classifier.py
@@ -1,14 +1,9 @@
-import requests,os
-from bs4 import BeautifulSoup
-import json
-import re
-import roman
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, classification_report
 import os
-import nltk
-import csv
-from nltk.corpus import stopwords
-nltk.download('stopwords')
-
 class Classifier:
 
     def __init__(self):
@@ -18,6 +13,25 @@ class Classifier:
         pass
     
     def define_classifier():
+        file_path = os.path.join(os.path.dirname(__file__), '../classifier_dataset.csv')
+        df = pd.read_csv(file_path)  
+        X = df['text']
+        y = df['query_type']
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+        vectorizer = TfidfVectorizer()
+        X_train_tfidf = vectorizer.fit_transform(X_train)
+        X_test_tfidf = vectorizer.transform(X_test)
+        classifier = LogisticRegression(max_iter=1000)
+        classifier.fit(X_train_tfidf, y_train)
+        predictions = classifier.predict(X_test_tfidf)
+        accuracy = accuracy_score(y_test, predictions)
+        print(f"Accuracy: {accuracy:.2f}")
+        print("Classification Report:\n", classification_report(y_test, predictions))
+        while True:
+            user_input = input("Enter a paragraph summary for book_title classification: ")
+            user_input_tfidf = vectorizer.transform([user_input])
+            predicted_genre = classifier.predict(user_input_tfidf)
+            print(f"Predicted Genre: {predicted_genre[0]}")
         pass
                 
 
--- chatbot/services/scraping.py
+++ chatbot/services/scraping.py
@@ -7,6 +7,7 @@ import os
 import nltk
 import csv
 from nltk.corpus import stopwords
+from chatbot.services.classifier import Classifier
 nltk.download('stopwords')
 
 class Scraping:
@@ -31,29 +32,20 @@ class Scraping:
                 temp_list.remove(each_word)
         return temp_list
     
-    def trim_stop_words(token_list):
+    def trim_stop_words(paragraph):
         stopwords_list = stopwords.words('english')
-        output_token_list = []
-        for word in token_list:
+        words_list = paragraph.split(' ')
+        output_word_list = []
+        for word in words_list:
             if word not in stopwords_list and len(word)>0:
-                output_token_list.append(word)
-        return output_token_list
+                output_word_list.append(word)
+        paragraph = " ".join(output_word_list)
+        return paragraph
 
-    def extract_novel_texts(novel_text):
-        text_data = novel_text.split('\n')
-        tokens=[]
-        for each_line in text_data:
-            each_line = re.split(r'\t+', each_line.rstrip('\t'))
-            line =  each_line.rstrip('\n')
-            line = Scraping.lowerCase(line)
-            line = Scraping.trim_extra_characters(line)
-            tokenized_line = Scraping.whitespace_tokenize(line)
-            token_list = Scraping.trim_stop_words(tokenized_line)        
-            tokens.extend(token_list)
-        return tokens
-
-    def initialize_scraping():
-        dataset=[["book_title","paragraph"]] 
+    def dataset_preparation():
+        novel_dataset=[["book_title","paragraph"]] 
+        classifier_dataset=[["query_type","text"]]
+        actual_docs = [["book_title","paragraph"]]
         folder_path = os.path.join(os.path.dirname(__file__), '../static/')
         files = os.listdir(folder_path)
         for file in files:
@@ -65,17 +57,45 @@ class Scraping:
                 paragraph = paragraph.strip()
                 paragraph = Scraping.trim_extra_characters(paragraph)
                 paragraph = Scraping.lowerCase(paragraph)
+                actual_docs.append([file,paragraph])
+                paragraph = Scraping.trim_stop_words(paragraph)
                 if len(paragraph)!=0:
                     row = []
+                    another_row=[]
                     row.append(file)
                     row.append(paragraph)
-                    dataset.append(row)
-        
-        csv_file_path = os.path.join(os.path.dirname(__file__), '../dataset.csv')
+                    novel_dataset.append(row)
+                    another_row.append("novel")
+                    another_row.append(paragraph)
+                    classifier_dataset.append(another_row)
+        csv_file_path = os.path.join(os.path.dirname(__file__), '../chitchat_conversations1.csv')
+        with open(csv_file_path, 'r') as csvfile:
+            csv_reader = csv.reader(csvfile)
+            for row in csv_reader:
+                title,text = row
+                if(title == "query_type"):
+                    continue
+                if(len(text)>0):
+                    classifier_dataset.append([title,text])
+        csv_file_path = os.path.join(os.path.dirname(__file__), '../novel_dataset.csv')
         with open(csv_file_path, 'w', newline='') as csvfile:
             csv_writer = csv.writer(csvfile)
-            for row in dataset:
+            for row in novel_dataset:
                 csv_writer.writerow(row)
+        csv_file_path = os.path.join(os.path.dirname(__file__), '../docs_dataset.csv')
+        with open(csv_file_path, 'w', newline='') as csvfile:
+            csv_writer = csv.writer(csvfile)
+            for row in actual_docs:
+                csv_writer.writerow(row)
+        csv_file_path2 = os.path.join(os.path.dirname(__file__), '../classifier_dataset.csv')
+        with open(csv_file_path2, 'w', newline='') as csvfile:
+            csv_writer = csv.writer(csvfile)
+            for row in classifier_dataset:
+                csv_writer.writerow(row)
+        
+    def initialize_scraping():
+        # Scraping.dataset_preparation()
+        Classifier.define_classifier()
         pass
                 
 
