37acd408c8daec6942c2e6a523674e0c92daa513
kukkadapudheeraj@gmail.com <kukkadapudheeraj>
2023-12-02 19:03:49 -0500

    added cors

--- chatbot/__init__.py
+++ chatbot/__init__.py
@@ -1,9 +1,10 @@
 from flask import Flask
 from .routes.base import base
+from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
-
+    CORS(app)
     route_files = ["base"]
     app.register_blueprint(base, url_prefix = '/')
 
